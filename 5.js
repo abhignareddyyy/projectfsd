c19ee3ee55dc3fffb7f24d406baf43a287152798
Amulya <kokkulaamulya4@gmail.com>
2025-12-18 09:16:19 +0530

    adding js files

---
+++ 5.js
@@ -0,0 +1,13 @@
+const p2 =
+{
+    name : "Abhi",
+    age : 21,
+    place:"Hyd"
+}
+console.log(p2)
+p2.age = 40;
+console.log(p2)
+p2.subject = "Hindi"
+console.log(p2)
+delete p2.age
+console.log(p2)
